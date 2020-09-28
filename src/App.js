56efbf088602bdc217d9a335942405e81ea96636
A K <ayaka.a3611@gmail.com>
2020-09-28 15:58:18 +0900

    learn jsx Please enter the commit message for your changes. Lines starting

--- src/App.js
+++ src/App.js
@@ -2,7 +2,16 @@ import React from 'react';
 
 
 function App() {
-  return <div><h1> Learn React 20200925</h1></div>;
+	// const greeting = "Hi, Tom!";
+	//const dom =<h1 className ="foo">{greeting}</h1>;
+	
+	return(
+	 <React.Fragment>
+	  <label htmlFor="bar">bar</label>
+	  <input type ="text" onChange={() => {console.log("I am clicked.")} } />
+	 </React.Fragment>
+	)
 }
+
 
 export default App;
