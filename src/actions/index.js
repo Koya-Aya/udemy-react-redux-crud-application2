f10f3947e49119964ff10e3f9611c467dc7c9a68
A K <ayaka.a3611@gmail.com>
2020-10-07 11:43:38 +0900

    event view

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,14 @@
+import axios from 'axios'
 
-export const INCREMENT='INCREMENT'
-export const DECREMENT='DECREMENT'
+export const READ_EVENTS='READ_EVENTS'
 
+const ROOT_URL ='https://udemy-utils.herokuapp.com/api/v1'
+const QUERYSTRING = '?token=token123'
 
-export const increment = () =>({
- type: INCREMENT
-})
+export const readEvents = () => async dispatch =>{
+ const response = await axios.get(`${ROOT_URL}/events${QUERYSTRING}`)
+ console.log(response)
+ dispatch({type: READ_EVENTS, response})
+}
 
- export const decrement = () =>({
-  type: DECREMENT
-})
 
